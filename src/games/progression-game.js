2ed1f978ced99b804f39e79588463e7ccf06c61e
nidges <nidges@yandex.ru>
2021-08-14 16:38:27 +0300

    linter errors fix

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,32 +1,30 @@
 export const getRandomProgression = () => {
-    const numberOfProgressionElements = Math.floor(Math.random() * 6) + 5;
-    //5 so it wouldn't be too difficult to count. '+1' is needed for it not to be zero
-    const progressionBase = Math.floor(Math.random() * 5) + 1;
-    const firstElement = Math.floor(Math.random() * 101);
-    const progressionNumbers = [firstElement];
+  const numberOfProgressionElements = Math.floor(Math.random() * 6) + 5;
+  // 5 so it wouldn't be too difficult to count. '+1' is needed for it not to be zero
+  const progressionBase = Math.floor(Math.random() * 5) + 1;
+  const firstElement = Math.floor(Math.random() * 101);
+  const progressionNumbers = [firstElement];
 
-    for (let i = 1; i < numberOfProgressionElements; i += 1) {
+  for (let i = 1; i < numberOfProgressionElements; i += 1) {
+    progressionNumbers.push(progressionNumbers[i - 1] + progressionBase);
+  }
 
-        progressionNumbers.push(progressionNumbers[i - 1] + progressionBase);
-    }
+  const randomMissingElementIndex = Math.floor(Math.random() * (numberOfProgressionElements - 1));
 
-    const randomMissingElementIndex = Math.floor(Math.random() * (numberOfProgressionElements - 1));
+  progressionNumbers[randomMissingElementIndex] = '..';
 
-    progressionNumbers[randomMissingElementIndex] = '..';
-
-    return progressionNumbers.join(' ');
-}
+  return progressionNumbers.join(' ');
+};
 
 export const getMissingElement = (string) => {
-    const progressionNumbers = string.split(' ');
-    const missingElementIndex = progressionNumbers.indexOf('..');
-    let progressionBase;
+  const progressionNumbers = string.split(' ');
+  const missingElementIndex = progressionNumbers.indexOf('..');
+  let progressionBase;
 
-    if (missingElementIndex > 1) {
-        progressionBase = Number(progressionNumbers[1]) - Number(progressionNumbers[0]);
-        return String(Number(progressionNumbers[missingElementIndex - 1]) + progressionBase);
-    } else {
-        progressionBase = Number(progressionNumbers[3]) - Number(progressionNumbers[2]);
-        return String(Number(progressionNumbers[missingElementIndex + 1]) - progressionBase);
-    }
-}
+  if (missingElementIndex > 1) {
+    progressionBase = Number(progressionNumbers[1]) - Number(progressionNumbers[0]);
+    return String(Number(progressionNumbers[missingElementIndex - 1]) + progressionBase);
+  }
+  progressionBase = Number(progressionNumbers[3]) - Number(progressionNumbers[2]);
+  return String(Number(progressionNumbers[missingElementIndex + 1]) - progressionBase);
+};
