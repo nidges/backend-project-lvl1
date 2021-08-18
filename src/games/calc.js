63064124099230450c423a56548987084b3f4d5e
nidges <nidges@yandex.ru>
2021-08-18 10:38:05 +0300

    calc.js: refactoring

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,6 +1,26 @@
 import wrapDataInAGame from '../index.js';
 import getRandomIntFromTo from '../utils.js';
 
+const calcAnswer = (firstOperand, operator, secondOperand) => {
+  let answer;
+
+  switch (operator) {
+    case '+':
+      answer = firstOperand + secondOperand;
+      break;
+    case '-':
+      answer = firstOperand - secondOperand;
+      break;
+    case '*':
+      answer = firstOperand * secondOperand;
+      break;
+    default:
+      break;
+  }
+
+  return String(answer);
+}
+
 const generateCalcData = () => {
   const greeting = 'What is the result of the expression?';
 
@@ -12,24 +32,7 @@ const generateCalcData = () => {
     const secondOperand = getRandomIntFromTo(0, 30);
     const question = `${firstOperand} ${operator} ${secondOperand}`;
 
-    let answer;
-    switch (operator) {
-      case '+':
-        // this is the only case where type conversion is necessary
-        // nevertheless let's use it everywhere for better code readability
-        answer = Number(firstOperand) + Number(secondOperand);
-        break;
-      case '-':
-        answer = Number(firstOperand) - Number(secondOperand);
-        break;
-      case '*':
-        answer = Number(firstOperand) * Number(secondOperand);
-        break;
-      default:
-        break;
-    }
-
-    answer = String(answer);
+    const answer = calcAnswer(firstOperand, operator, secondOperand);
 
     return { question, answer };
   };
